c47785974ff9eda1de88306f31fa02e97153b6e8
Bob Tian <Bob.Tian@carleton.ca>
2021-03-21 18:18:45 -0400

    write up a simple battery

--- battery.cpp
+++ battery.cpp
@@ -4,3 +4,36 @@ Battery::Battery()
 {
 
 }
+
+int Battery::get_remain_int() const{
+    return static_cast<int>(remain);
+}
+
+double Battery::get_remain_double() const{
+    return remain;
+}
+
+double Battery::drain(double amount) {
+    remain -= amount;
+    fix();
+    return remain;
+}
+
+double Battery::charge(double amount) {
+    remain += amount;
+    fix();
+    return remain;
+}
+
+void Battery::set(double amount) {
+    remain = amount;
+    fix();
+}
+
+void Battery::fix() {
+    if (remain > capacity){
+        remain = capacity;
+    }else if (remain < 0){
+        remain = 0;
+    }
+}
--- battery.h
+++ battery.h
@@ -7,10 +7,18 @@ class Battery: public QWidget
     Q_OBJECT
 public:
     Battery();
+    int get_remain_int() const;
+    double get_remain_double() const;
+    double drain(double amount);
+    double charge(double amount);
+    void set(double amout);
+
 
 private:
-    int capacity;
-    int remain;
+    const double capacity = 100;
+    double remain;
+    double screen_power_consumption = 0.1;
+    void fix();
 };
 
 #endif // BATTERY_H
