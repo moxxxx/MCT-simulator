c30cdb9ffa870ee882897897d0f713656be4da3d
Bob Tian <Bob.Tian@carleton.ca>
2021-03-23 13:58:36 -0400

    Change naming convention

--- treatmentprogram.h
+++ treatmentprogram.h
@@ -7,16 +7,16 @@ struct TreatmentProgram: public Program
 public:
     TreatmentProgram(QString title, int frequency);
     TreatmentProgram(QString title, int frequency, int time);
-    void set_power_level(int power_level);
-    QString get_title();
-    int get_frequency();
-    int get_power_level();
-    int get_time();
+    void setPowerLevel(int powerLevel);
+    QString getTitle();
+    int getFrequency();
+    int getPowerLevel();
+    int getTime();
 
 private:
     QString title;
     int frequency;
-    int power_level;
+    int powerLevel;
     int time;
 
 };
